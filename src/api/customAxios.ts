995e01585ef51a4a1c78b60319bf2deb85cfb62b
Oscar <oscargm28@hotmail.com>
2022-12-17 19:42:46 +0100

    changed customAxios

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -7,11 +7,13 @@ axios.defaults.withCredentials = true;
 const userCookie = Cookies.get("access_token");
 
 export const customAxios = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://yt-clone-mern.onrender.com/api",
 });
 
 export const customAxiosWithToken = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://yt-clone-mern.onrender.com/api",
   headers: {
     Authorization: "Bearer " + userCookie,
   },
